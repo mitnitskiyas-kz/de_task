583274f02abb038d7109c34e09fcf0b4b6780357
Aleksei_Mitnitskii <Aleksei_Mitnitskii@epam.com>
2023-10-26 23:09:26 +0600

    final attempt

--- src/de_task/structure.py
+++ src/de_task/structure.py
@@ -9,15 +9,14 @@ olist_customers_dataset_schema = StructType([
     StructField('customer_unique_id', StringType(), True),
     StructField('customer_zip_code_prefix', StringType(), True),
     StructField('customer_city', StringType(), True),
-    StructField('customer_state', StringType(), True)
-    ])
+    StructField('customer_state', StringType(), True)])
+
 olist_geolocation_dataset_schema = StructType([
     StructField('geolocation_zip_code_prefix', StringType(), True),
     StructField('geolocation_lat', DoubleType(), True),
     StructField('geolocation_lng', DoubleType(), True),
     StructField('geolocation_city', StringType(), True),
-    StructField('geolocation_state', StringType(), True)
-    ])
+    StructField('geolocation_state', StringType(), True)])
 
 olist_order_items_dataset_schema \
     = StructType([StructField('order_id', StringType(), True),
@@ -26,16 +25,14 @@ olist_order_items_dataset_schema \
                   StructField('seller_id', StringType(), True),
                   StructField('shipping_limit_date', TimestampType(), True),
                   StructField('price', DoubleType(), True),
-                  StructField('freight_value', DoubleType(), True)
-                  ])
+                  StructField('freight_value', DoubleType(), True)])
 
 olist_order_payments_dataset_schema \
     = StructType([StructField('order_id', StringType(), True),
                   StructField('payment_sequential', IntegerType(), True),
                   StructField('payment_type', StringType(), True),
                   StructField('payment_installments', IntegerType(), True),
-                  StructField('payment_value', DoubleType(), True)
-                  ])
+                  StructField('payment_value', DoubleType(), True)])
 
 olist_order_reviews_dataset_schema \
     = StructType([StructField('review_id', StringType(), True),
@@ -44,8 +41,7 @@ olist_order_reviews_dataset_schema \
                   StructField('review_comment_title', StringType(), True),
                   StructField('review_comment_message', StringType(), True),
                   StructField('review_creation_date', TimestampType(), True),
-                  StructField('review_answer_timestamp', TimestampType(), True)
-                  ])
+                  StructField('review_answer_timestamp', TimestampType(), True)])
 
 
 olist_orders_dataset_schema = StructType([
@@ -56,8 +52,7 @@ olist_orders_dataset_schema = StructType([
     StructField('order_approved_at', TimestampType(), True),
     StructField('order_delivered_carrier_date', TimestampType(), True),
     StructField('order_delivered_customer_date', TimestampType(), True),
-    StructField('order_estimated_delivery_date', TimestampType(), True)
-    ])
+    StructField('order_estimated_delivery_date', TimestampType(), True)])
 
 olist_products_dataset_schema = StructType([
     StructField('product_id', StringType(), True),
@@ -66,17 +61,14 @@ olist_products_dataset_schema = StructType([
     StructField('product_description_lenght', IntegerType(), True),
     StructField('product_weight_g', IntegerType(), True),
     StructField('product_length_cm', IntegerType(), True),
-    StructField('product_height_cm', IntegerType(), True)
-    ])
+    StructField('product_height_cm', IntegerType(), True)])
 
 olist_sellers_dataset_schema = StructType([
     StructField('seller_id', StringType(), True),
     StructField('seller_zip_code_prefix', StringType(), True),
     StructField('seller_city', StringType(), True),
-    StructField('seller_state', StringType(), True)
-    ])
+    StructField('seller_state', StringType(), True)])
 
 product_category_name_translation_schema = StructType([
     StructField('product_category_name', StringType(), True),
-    StructField('product_category_name_english', StringType(), True)
-    ])
+    StructField('product_category_name_english', StringType(), True)])
